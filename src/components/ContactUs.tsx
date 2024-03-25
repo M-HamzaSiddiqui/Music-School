e4107b3038d0ffffc30079073462050bd4eb0724
M-HamzaSiddiqui <userhamza01@gmail.com>
2024-03-25 14:59:24 +0530

    minor css fix in contactus page

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 function ContactUs() {
   return (
-    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+    <div className=" h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold mt-20 mb-5">
           Contact Us
         </h1>
 
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10 mt-2">
-          We`&apos;`re here to help with any questions about our courses, programs, or events. Reach out and let us know how we can assist you in your musical journey.
+          We&apos;re here to help with any questions about our courses, programs, or events. Reach out and let us know how we can assist you in your musical journey.
         </p>
         <input
           type="text"
